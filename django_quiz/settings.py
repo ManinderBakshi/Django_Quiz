3a707ed654884f3c7e0c6a760bf73f9cfa45e9b6
This PC <manibakshi26@gmail.com>
2020-08-15 16:05:32 +0530

    Initial COmmit

--- django_quiz/settings.py
+++ django_quiz/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -126,5 +125,3 @@ STATICFILES_DIRS = [
     STATIC_DIR,
 ]
 
-# Activate Django-Heroku.
-django_heroku.settings(locals())
